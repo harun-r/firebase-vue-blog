29319a2e3ffac7ecd9f14391736f0fd6299e1801
Harun <mdharunbd88@gmail.com>
2024-01-07 19:37:59 +0600

    Header design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
     fontFamily: {
-      roboto: ['Roboto Mono', 'monospace'],
+      sans: ['Roboto Mono', 'monospace'],
     },
+    extend:{
+      colors: {
+        'blue': '#1fb6ff',
+        'pink': '#ff49db',
+        'gray': '#8492a6',
+        'green': '#42b883'
+      },
+      fontSize: {
+        base: '.9rem',
+      },
+    }
   },
   plugins: [],
 }
