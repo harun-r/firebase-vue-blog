3d904fe8a03f0957003169fcaa4f8f8008d860ed
Harun <mdharunbd88@gmail.com>
2024-01-07 17:56:48 +0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# firebase-vue-blog
+# Firebase Vue Blog
+
+This project is for making a simple blog with vue, vuex and firebase.
+
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,12 @@
+import {initializeApp}  from "firebase/app";
+// import {firestore} from 'firebase/firestore'
+const config = {
+    apiKey: "AIzaSyAuixjL3_Vg-_PmkoFS0CU_Pu-bG8yC0HU",
+    authDomain: "fir-vueblog-146eb.firebaseapp.com",
+    projectId: "fir-vueblog-146eb",
+    storageBucket: "fir-vueblog-146eb.appspot.com",
+    messagingSenderId: "1072304344487",
+    appId: "1:1072304344487:web:74cc6679cab392186ffa82"
+};
+
+initializeApp(config);
